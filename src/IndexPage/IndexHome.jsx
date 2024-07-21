0bffb53d882fbcc599a92adf6c63bd17510c79cc
bishald0013 <bishal986473@gmail.com>
2024-07-21 11:45:11 +0530

    new ui added

--- src/IndexPage/IndexHome.jsx
+++ src/IndexPage/IndexHome.jsx
@@ -1,6 +1,4 @@
-import React from 'react'
 import './IndexHome.css'
-import UserData from './UserData';
 
 
 function IndexHome() {
@@ -14,14 +12,41 @@ function IndexHome() {
           customers.
         </span>
       </div>
-      <div className="container my-5 mainText">
-        <span className='fs-5'>
-          Hii, Bishal Deb
-        </span>
-      </div>
-      <div className="container">
-        <div className="card">
-          <UserData/>
+      <div className="container w-50">
+        <p className='my-4 infoT fs-5'>Your Details</p>
+        <div className="row mt-4">
+          <div className="col-lg-6">
+            <div className="card rounded-4 border-success-subtle shadow">
+              <div className="mt-3">
+               <span className='infoText1'> Your Alerts </span>
+              </div>
+              <div className="mt-2 ">
+               <span className='infoText2 fw-light fst-italic'>Total Alerts :- 20</span>
+              </div>
+              <div className="">
+               <span className='infoText2 fw-light fst-italic'>Pending :- 10</span>
+              </div>
+              <div className="mb-4">
+               <span className='infoText2 fw-light fst-italic'>Sent :- 10</span>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-6">
+              <div className="card rounded-4 border-success-subtle shadow">
+                <div className="mt-3">
+                <span className='infoText1'> Registered Vehicles </span>
+                </div>
+                <div className="mt-2 ">
+                <span className='infoText2 fw-light fst-italic'>AS11D3456</span>
+                </div>
+                <div className="">
+                <span className='infoText2 fw-light fst-italic'>AS11D3456</span>
+                </div>
+                <div className="mb-4">
+                <span className='infoText2 fw-light fst-italic'>AS11D3456</span>
+                </div>
+              </div>
+          </div>
         </div>
       </div>
     </div>
