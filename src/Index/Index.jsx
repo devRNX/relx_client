62ccacf56c04a55c2931c0ebce541b014357483e
Biptu Das <c.biptu.pc.mail@gmail.com>
2024-08-18 21:52:43 +0530

    removed about us upper section

--- src/Index/Index.jsx
+++ src/Index/Index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import IndexContent from "./IndexContent";
 import Services from "../components/Services/Services";
-import AboutUs from "../components/AboutUs/AboutUs";
 
 function Index() {
   return (
@@ -9,7 +8,6 @@ function Index() {
       <div className="container">
         <IndexContent />
         <Services />
-        <AboutUs />
       </div>
     </div>
   );
